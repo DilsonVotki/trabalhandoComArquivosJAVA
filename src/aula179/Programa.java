d534691ce8ba0e9b0137667e5b1e6ee4ac16349e
Dilson Votki <dilsonpl@hotmail.com>
2020-04-08 00:47:09 -0300

    commit aula 179 - Bloco try-with-resources

---
+++ src/aula179/Programa.java
@@ -0,0 +1,27 @@
+package aula179;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Programa {
+
+	public static void main(String[] args) {
+		
+		String pasta = "c:\\temp\\in.txt";
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(pasta))) {
+			String linha = br.readLine();
+			
+			while (linha != null) {
+				System.out.println(linha);
+				linha = br.readLine();
+			}
+		}
+		catch (IOException e){
+			System.out.println("Erro: " + e.getMessage());
+		}
+		
+	}
+
+}
